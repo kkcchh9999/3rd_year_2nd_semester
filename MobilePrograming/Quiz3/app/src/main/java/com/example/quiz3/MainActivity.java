6d0aed750289e571c7f0ad5e6db050953ff2db29
권철현 <kkcchh9999@naver.com>
2021-09-08 13:08:12 +0900

    모바일 프로그래밍 Quiz3

---
+++ MobilePrograming/Quiz3/app/src/main/java/com/example/quiz3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.quiz3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view ->
+                Toast.makeText(getApplicationContext(),"권철현",Toast.LENGTH_SHORT).show()
+        );
+    }
+}
