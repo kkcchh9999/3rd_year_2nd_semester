1b2b8cfa6417bdeab342d4b62b7404a1d616e421
권철현 <kkcchh9999@naver.com>
2021-09-09 20:06:14 +0900

    모바일 프로그래밍 Quiz4 - 예외처리

--- MobilePrograming/Quiz4/app/src/main/java/com/example/quiz4/MainActivity.java
+++ MobilePrograming/Quiz4/app/src/main/java/com/example/quiz4/MainActivity.java
@@ -24,14 +24,16 @@ public class MainActivity extends AppCompatActivity {
         secondInput = findViewById(R.id.second_input);
         addButton = findViewById(R.id.button_add);
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int result = 0;
-                result = Integer.parseInt(String.valueOf(firstInput.getText()))
-                        + Integer.parseInt(String.valueOf(secondInput.getText()));
+        addButton.setOnClickListener(view -> {
+            int result = 0;
+
+            try {
+                    result = Integer.parseInt(String.valueOf(firstInput.getText()))
+                            + Integer.parseInt(String.valueOf(secondInput.getText()));
 
                 Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "올바른 정수를 입력하세요", Toast.LENGTH_SHORT).show();
             }
         });
     }
