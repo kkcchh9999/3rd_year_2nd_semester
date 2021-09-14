29875b9a6a0e2f55e717be578abe12cbe05b4d6c
권철현 <kkcchh9999@naver.com>
2021-09-14 13:41:58 +0900

    모바일 프로그래밍 Quiz4

--- MobilePrograming/Quiz4/app/src/main/java/com/example/quiz4/MainActivity.java
+++ MobilePrograming/Quiz4/app/src/main/java/com/example/quiz4/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     EditText firstInput;
     EditText secondInput;
     ImageButton addButton;
+    Button devButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +25,27 @@ public class MainActivity extends AppCompatActivity {
         firstInput = findViewById(R.id.first_input);
         secondInput = findViewById(R.id.second_input);
         addButton = findViewById(R.id.button_add);
+        devButton = findViewById(R.id.button_dev);
 
         addButton.setOnClickListener(view -> {
-            int result = 0;
+            double result = 0;
 
             try {
-                    result = Integer.parseInt(String.valueOf(firstInput.getText()))
-                            + Integer.parseInt(String.valueOf(secondInput.getText()));
+                result = Double.parseDouble(String.valueOf(firstInput.getText()))
+                        + Double.parseDouble(String.valueOf(secondInput.getText()));
+
+                Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "올바른 정수를 입력하세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        devButton.setOnClickListener(view -> {
+            double result = 0;
+
+            try {
+                result = Double.parseDouble(String.valueOf(firstInput.getText()))
+                        / Double.parseDouble(String.valueOf(secondInput.getText()));
 
                 Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_SHORT).show();
             } catch (Exception e) {
