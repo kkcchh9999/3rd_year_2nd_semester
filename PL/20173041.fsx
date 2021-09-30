627d4e90b485930de2ea4f2e0e522cb1b449fc35
권철현 <kkcchh9999@naver.com>
2021-10-01 01:02:34 +0900

    PL 과제

---
+++ PL/20173041.fsx
@@ -0,0 +1,7 @@
+let double x = 2 * x
+let add1 x = x + 1
+let zero = 0
+
+let my = (add1(double(double(double(double(add1(double(add1(double(add1(double(add1(double(double(double(double(double(add1(double(double(add1(double(add1(double(add1(double(add1(double(double(double(add1(double(add1(double(double(double((add1 zero))))))))))))))))))))))))))))))))))))))
+
+printfn "%d" my
---
+++ PL/Hello.fsx
@@ -0,0 +1,3 @@
+printfn "Hello World from F#"
+let square x = x * x
+printfn "%d" (square 5)
