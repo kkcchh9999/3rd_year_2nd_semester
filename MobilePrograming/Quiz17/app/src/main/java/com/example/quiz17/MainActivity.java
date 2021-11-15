acd6a57486f30f2d6c1b628d2b7a8e70376f5485
kkcchh9999 <kkcchh9999@naver.com>
2021-11-15 14:20:29 +0900

    Quiz 17

---
+++ MobilePrograming/Quiz17/app/src/main/java/com/example/quiz17/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.quiz17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    Button button;
+    EditText editText;
+    String fileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 일기장(권철현)");
+
+        datePicker = findViewById(R.id.datePicker);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        fileName = Integer.toString(year) + "_"
+                                + Integer.toString(monthOfYear +1) + "+"
+                                + Integer.toString(dayOfMonth) + ".txt";
+
+                        String str = readDiary(fileName);
+                        editText.setText(str);
+                        button.setEnabled(true);
+                    }
+                });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream outFs = openFileOutput(fileName,
+                            Context.MODE_PRIVATE);
+                    String str = editText.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), fileName + "이 저장됨", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private String readDiary(String fName) {
+        String diaryStr = null;
+        FileInputStream inFs;
+        try {
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            diaryStr = (new String(txt)).trim();
+            button.setText("수정하기");
+
+        } catch (Exception e) {
+            editText.setHint("일기 없음");
+            button.setText("새로 저장");
+        }
+        return diaryStr;
+    }
+}
