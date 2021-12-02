e93a6e50518f213c20a91a124235549deb938cd5
kkcchh9999 <kkcchh9999@naver.com>
2021-12-02 11:18:15 +0900

    복호화 완성

--- InformationSecurity/Proj/src/Decription.kt
+++ InformationSecurity/Proj/src/Decription.kt
@@ -1,119 +1,176 @@
 
 fun main(){
-
     //암호문, 키 입력받기
     print("암호문을 입력하세요 : ")
     val password = readLine()
     print("키를 입력하세요 : ")
     val key = readLine()
-    var keySort = mutableListOf<Char>()
 
-    //키의 모음 갯수 체크
-    var vowelCount = 0
-    val vowel = arrayOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-    for (i in key!!.indices) {
-        if (vowel.contains(key[i])) {
-            vowelCount ++
+    if (key == null || password == null) {
+        println("평문, 키를 입력하세요")
+        return
+    }
+
+    //암호문 이진화
+    var binaryPassword = ""
+    for (i in password.indices) {
+        var tmpPass: String = Integer.toBinaryString(password[i]-'0')
+        while (tmpPass.length < 2) {
+            tmpPass = ("0$tmpPass")
         }
-        //키 순서를 통해 정렬을 위함
+        binaryPassword += tmpPass
+    }
+    println("binaryP: $binaryPassword")
+
+    //키 아스키코드
+    var codeKey = ""
+    for (i in key.indices) {
+        codeKey += key[i].code
+    }
+
+    //아스키코드 키 이진화, 길이 늘려서 암호문과 같게
+    var binaryKey = ""
+    var tmpBinaryKey = ""
+    for (i in codeKey.indices) {
+        binaryKey = Integer.toBinaryString(codeKey[i]-'0')
+        while(binaryKey.length < 4) {
+            binaryKey = "0$binaryKey"
+        }
+        tmpBinaryKey += binaryKey
+    }
+    binaryKey = ""
+    var tmpStart = 0
+    for (i in binaryPassword.indices) {
+        binaryKey += tmpBinaryKey[tmpStart]
+        if (tmpStart < tmpBinaryKey.length-1) {
+            tmpStart ++
+        } else {
+            tmpStart = 0
+        }
+    }
+    println("binaryK: $binaryKey")
+    //키 평문 xor
+    var xorPass = ""
+    for (i in binaryPassword.indices) {
+        xorPass += if (binaryPassword[i] == binaryKey[i]) {
+            0
+        } else {
+            1
+        }
+    }
+    println("xorPass: $xorPass")
+
+    var codePass = ""
+    for (i in xorPass.indices step 4) {
+        val tmp = ((xorPass[i]-'0') * 8 + (xorPass[i+1]-'0') * 4 + (xorPass[i+2]-'0') * 2 + (xorPass[i+3]-'0'))
+        codePass += tmp
+    }
+    println("아스키 코드: $codePass")
+    var strPass = ""
+    for (i in codePass.indices step 2) {
+        val tmp: Char = (((codePass[i]-'0') * 10 + (codePass[i+1]-'0')).toChar())
+        strPass += tmp
+    }
+    println("전치된 평문: $strPass")
+
+    //키 정렬
+    val keySort = mutableListOf<Char>()
+    for (i in key.indices) {
         keySort.add(key[i])
     }
+    keySort.sort()
+    println("정렬된 키: $keySort")
+    //a e l p p
+    //a p p l e
+    //1 4 5 3 2
 
-    //비즈네르 암호
-    var vigenere = Array<CharArray>(26) { CharArray(26) }
-    var alphabet = 'A'
-    for (i in 0..25) {
-        for (j in 0..25) {
-            vigenere[i][j] = alphabet
-            alphabet ++
-            if (alphabet > 'Z') {
-                alphabet = 'A'
+    val keySequence: MutableList<Int> = emptyList<Int>().toMutableList()
+    for (i in key.indices) {
+        for (j in keySort.indices) {
+            if (key[i] == keySort[j]) {
+                keySequence.add(j)
+                keySort[j]='_'
             }
         }
-        alphabet++
+    }
+    //키 순서?
+    println("키 순서: $keySequence")
+
+
+    var startPoint = 0
+    var endPoint = key.length-1
+    var tmpString = ""
+    var sortPass = ""
+    val keyDev = if (strPass.length % key.length == 0) {
+        strPass.length / key.length
+    } else {
+        strPass.length / key.length + 1
     }
 
-    //비즈네르 암호 복호화
-    var passwordBeforeVigenere: String = ""
-    var keyCount = 0
-    for (i in password!!.indices) {
-        if (password[i] == ' ') {
-            passwordBeforeVigenere += ' '
-        } else {
-            for (j in 0..25) {
-                if (key[keyCount] in 'a'..'z') {
-                    if (vigenere[key[keyCount].toInt()-97][j].toLowerCase() == password[i]) {
-                        passwordBeforeVigenere += (j + 97).toChar()
-                    }
-                } else if (key[keyCount] in 'A'..'Z') {
-                    if (vigenere[key[keyCount].toInt()-65][j] == password[i]) {
-                        passwordBeforeVigenere += (j + 65).toChar()
+    for (i in 0 until keyDev) {//전치해서 추가 키 자리수로 나누어 떨어지는 부분들에 대해서
+        if (endPoint < strPass.length) {
+            tmpString = strPass.substring(startPoint, endPoint+1)
+            //21403
+            //BCUGX XJI
+            //GCBXU JXI
+
+            val tmpCharArray = arrayOfNulls<Char>(tmpString.length)
+            for (j in tmpString.indices) {
+                for (k in keySequence.indices) {
+                    if (keySequence[k] == j) {
+                        tmpCharArray[k]=tmpString[j]
+                        break
                     }
                 }
             }
-        }
-        keyCount ++
-        if (keyCount == key.length) {
-            keyCount = 0
-        }
-    }
-    println("비즈네르 암호 복호화: [$passwordBeforeVigenere]")
-
-    //암호문 배열에 세로로 넣기
-    var passwordArray = Array<CharArray>(password!!.length / key.length) { CharArray(key.length) { _ -> ' ' } }
-    var countRow = 0
-    var countColumn = 0
-    for (i in passwordBeforeVigenere.indices) {
-        passwordArray[countColumn][countRow] = passwordBeforeVigenere[i]
-        countColumn ++
-        if (countColumn == password.length / key.length) {
-            countColumn = 0
-            countRow ++
-        }
-    }
-    println("세로로 배열에 저장 : ${passwordArray.contentDeepToString()}")
-
-
-    //키 순서 맵핑
-    keySort.sort()
-    val keyMap = mutableMapOf<Int, Char>()
-    for (i in keySort.indices) {
-        keyMap[i] = keySort[i]
-    }
-
-    var shiftInput = ""
-    countRow = 0
-    countColumn = 0
-    for (i in passwordBeforeVigenere.indices) {
-        for (j in key.indices) {
-            if (key[countRow] == keyMap[j]) {
-                shiftInput += passwordArray[countColumn][j]
+            for (j in tmpCharArray.indices) {
+                sortPass += tmpCharArray[j]
             }
+        } else {//마지막부분 키 남은만큼 짤라서
+            tmpString = strPass.substring(startPoint)
+            val tmpKey = key.substring(0, strPass.length % key.length)
+            val tmpKeySort: MutableList<Char> = emptyList<Char>().toMutableList()
+            for (i in tmpKey.indices) {
+                tmpKeySort.add(tmpKey[i])
+            }
+            tmpKeySort.sort()
+            //다시 정렬하고
+            val tmpKeySequence: MutableList<Int> = emptyList<Int>().toMutableList()
+            for (i in tmpKey.indices) {
+                for (j in tmpKeySort.indices) {
+                    if (tmpKey[i] == tmpKeySort[j]) {
+                        tmpKeySequence.add(j)
+                        tmpKeySort[j]='_'
+                    }
+                }
+            }
+            //정렬한거 순서 찾아서
+            val tmpCharArray = arrayOfNulls<Char>(tmpString.length)
+            for (j in tmpString.indices) {
+                for (k in tmpKeySequence.indices) {
+                    if (tmpKeySequence[k] == j) {
+                        tmpCharArray[k]=tmpString[j]
+                        break
+                    }
+                }
+            }
+            for (j in tmpCharArray.indices) {
+                sortPass += tmpCharArray[j]
+            }
+            //적용하기
         }
-        countRow ++
-        if (countRow == key.length) {
-            countRow = 0
-            countColumn ++
-        }
+        startPoint += key.length
+        endPoint += key.length
     }
-    println("key 순서에 맞춰 배열에서 꺼내기 : $shiftInput")
+    println("시프트된 평문: $sortPass")
 
-    var input = ""
-    //키의 모음 갯수만큼 카운트 and 시프트
-    for (i in shiftInput.indices) {
-        if (shiftInput[i] in 'a'..'z') {
-            input += if (shiftInput[i] - vowelCount < 'a') {
-                (shiftInput[i]-vowelCount+26)
-            } else {
-                (shiftInput[i]-vowelCount)
-            }
-        } else if (shiftInput [i] in 'A'..'Z') {
-            input += if (shiftInput[i] - vowelCount < 'A') {
-                (shiftInput[i]-vowelCount+26)
-            } else {
-                (shiftInput[i]-vowelCount)
-            }
+    var planeText = ""
+    for (i in sortPass.indices) {
+        planeText += if (sortPass[i] - (key[i % key.length].code) % 26 !in 'A'..'Z') {
+            sortPass[i] - (key[i % key.length].code) % 26 + 26
+        } else {
+            sortPass[i] - (key[i % key.length].code) % 26
         }
     }
-    println("복구된 평문 : $input")
+    println("평문: $planeText")
 }
